440bdd502af09624ac762427a7ef2751a918cfa7
WilliamG99 <19willgarcia99@gmail.com>
2024-12-11 20:21:56 +0000

    project init
    Initial project set with some very basic page navigation added

---
+++ app/src/main/java/com/example/hc_marketapp/MainActivity.kt
@@ -0,0 +1,112 @@
+package com.example.hc_marketapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Button
+import androidx.compose.material3.Card
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.hc_marketapp.ui.theme.HC_marketappTheme
+
+// Compose Navigation
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+
+        setContent {
+            HC_marketappTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                    MyApp()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    HC_marketappTheme {
+        Greeting("Android")
+    }
+}
+
+
+@Composable
+fun MyApp() {
+    val navController = rememberNavController()
+    NavHost(
+        navController = navController,
+        startDestination = "main_page"
+    ) {
+        // Define the main page
+        composable("main_page") { MainPage(navController) }
+
+        // Define the second page
+        composable("second_page") { SecondPage() }
+    }
+}
+
+@Composable
+fun MainPage(navController: NavHostController) {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp),
+        verticalArrangement = Arrangement.Center
+    ) {
+        Button(
+            onClick = { navController.navigate("second_page") },
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            Text(text = "Go to Second Page")
+        }
+    }
+}
+
+@Composable
+fun SecondPage() {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp),
+        verticalArrangement = Arrangement.Center
+    ) {
+        Text(
+            text = "This is the second page!",
+            style = MaterialTheme.typography.headlineMedium
+        )
+    }
+}
