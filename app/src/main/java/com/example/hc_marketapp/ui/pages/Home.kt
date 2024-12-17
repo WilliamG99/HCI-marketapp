317220fdfe81d782ec83019c013b00db6a69c03d
WilliamG99 <19willgarcia99@gmail.com>
2024-12-17 23:39:17 +0000

    add pager for recent news in home page

--- app/src/main/java/com/example/hc_marketapp/ui/pages/Home.kt
+++ app/src/main/java/com/example/hc_marketapp/ui/pages/Home.kt
@@ -1,39 +1,159 @@
 package com.example.hc_marketapp.ui.pages
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.horizontalScroll
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.BoxWithConstraints
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.pager.HorizontalPager
+import androidx.compose.foundation.pager.rememberPagerState
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun Home(modifier: Modifier = Modifier) {
+    BoxWithConstraints(modifier = modifier.fillMaxSize()) {
+        //val boxWithConstraintsScope = this
+        val totalHeight = this.maxHeight
 
-    Column(
-        modifier = modifier.fillMaxSize(),
-        horizontalAlignment = Alignment.CenterHorizontally
-    ) {
-        Text("This is the Home Screen!")
-        Row (
+        Column(
             modifier = Modifier
-                .fillMaxWidth()
-                .weight(0.3f)
-                .background(MaterialTheme.colorScheme.primary)
+                .fillMaxSize()
+                .verticalScroll(rememberScrollState())
+                //.padding(10.dp)
         ) {
-            Text("A row inside page column")
-        }
-        Row (
-            modifier = Modifier
-                .fillMaxWidth()
-                .weight(0.7f)
-                .background(MaterialTheme.colorScheme.secondary)
-        ) {
-            Text("Another row inside page column")
+            // Recent News Sections
+            Column (
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(totalHeight * 0.5f)
+                    .padding(0.dp)
+            ) {
+
+                Text("Recent News", style = MaterialTheme.typography.headlineSmall)
+
+                // Display 10 items
+                val pagerState = rememberPagerState(pageCount = {
+                    10
+                })
+                HorizontalPager(state = pagerState) { page ->
+                    // Our page content
+                    Card(
+                        colors = CardDefaults.cardColors(
+                            containerColor = MaterialTheme.colorScheme.surfaceVariant,
+                        ),
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .padding(8.dp)
+                    ) {
+                        Text(
+                            text = "Article: $page",
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .padding(16.dp),
+                            textAlign = TextAlign.Center,
+                        )
+                    }
+                }
+
+                Row(
+                    modifier = Modifier
+                        .horizontalScroll(rememberScrollState())
+                        .background(MaterialTheme.colorScheme.primary),
+                    horizontalArrangement = Arrangement.spacedBy(8.dp)
+                ) {
+                    Card(
+                        colors = CardDefaults.cardColors(
+                                containerColor = MaterialTheme.colorScheme.surfaceVariant,
+                        ),
+                        modifier = Modifier
+                            .fillMaxHeight()
+                            .width(240.dp)
+                    ) {
+                        Text(
+                            text = "Filled",
+                            modifier = Modifier
+                                .padding(16.dp),
+                            textAlign = TextAlign.Center,
+                        )
+                    }
+
+                    Card(
+                        colors = CardDefaults.cardColors(
+                            containerColor = MaterialTheme.colorScheme.surfaceVariant,
+                        ),
+                        modifier = Modifier
+                            .size(width = 240.dp, height = 100.dp)
+                    ) {
+                        Text(
+                            text = "Another",
+                            modifier = Modifier
+                                .padding(16.dp),
+                            textAlign = TextAlign.Center,
+                        )
+                    }
+                }
+            }
+
+            // Markets Section
+            Column (
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(totalHeight * 0.5f) // 30% of screen height
+                    .padding(0.dp)
+            ) {
+                Text("Another row inside page column", style = MaterialTheme.typography.headlineSmall)
+
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .height(totalHeight * 0.5f)
+                        .background(MaterialTheme.colorScheme.secondary)
+                ) {
+
+
+                    // Display 10 items
+                    val pagerState = rememberPagerState(pageCount = {
+                        10
+                    })
+                    HorizontalPager(state = pagerState) { page ->
+                        // Our page content
+                        Text(
+                            text = "Page: $page",
+                            modifier = Modifier.fillMaxWidth()
+                        )
+                    }
+                }
+            }
+
+
+            repeat(20) { index ->
+                Card(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(8.dp),
+                    colors = CardDefaults.cardColors(containerColor = MaterialTheme.colorScheme.surfaceVariant)
+                ) {
+                    Text(
+                        "Item #$index",
+                        modifier = Modifier.padding(16.dp)
+                    )
+                }
+            }
         }
     }
 }
